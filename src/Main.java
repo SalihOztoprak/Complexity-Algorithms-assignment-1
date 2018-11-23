d04caa48eb3f2c341ed0468bfac5e6df98703978
thomastunc <thomastunc@hotmail.com>
2018-11-23 11:06:17 +0100

    Added timerecorders

--- src/Main.java
+++ src/Main.java
@@ -7,34 +7,20 @@ public class Main {
     public static void main(String[] args) {
 
        new Main().run();
-
     }
 
-    public void run() {
+    private void run() {
         int[] list = lG.numberList(10);
         System.out.println(Arrays.toString(list));
         list = sA.sortedMinMax(list);
         System.out.println(Arrays.toString(list));
     }
 
-    /**
-     * This method will sort a given array
-     *
-     * @param unsortedList the list that you want to sort
-     * @return the sorted list
-     */
-    private int[] sortedMinMax(int[] unsortedList) {
-        for (int i = 0; i < unsortedList.length; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < unsortedList.length; j++) {
-                if (unsortedList[j] < unsortedList[minIndex]) {
-                    minIndex = j;
-                }
-            }
-            int temp = unsortedList[i];
-            unsortedList[i] = unsortedList[minIndex];
-            unsortedList[minIndex] = temp;
-        }
-        return unsortedList;
+    private long startRecordingTime(){
+        return System.currentTimeMillis();
+    }
+
+    private long endRecordingTime(long startTime){
+        return System.currentTimeMillis() - startTime;
     }
 }
