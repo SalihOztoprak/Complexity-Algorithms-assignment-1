ded14e1192b5d86cae8f1b19b175235b09f054a3
Salih Oztoprak <salih.58@live.nl>
2018-11-23 10:53:13 +0100

    list generator and sorting done

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
+
 public class Main {
+    private NumberGenerator numberGenerator = new NumberGenerator();
     public static void main(String[] args) {
+
        new Main().run();
+
     }
 
     public void run(){
+        int [] list = numberGenerator.numberList(10);
 
+        for (int i = 0; i <list.length ; i++) {
+            System.out.println(list[i]);
+        }
     }
+
+
+
 }
